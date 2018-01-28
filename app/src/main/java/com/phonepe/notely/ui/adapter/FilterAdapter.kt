d39f636700152629ba0c4bd9970c91ab12521799
Kumar <kumar.gaurav.ciit@gmail.com>
2018-01-29 00:22:39 +0530

    applied data binding to filter adapter item view

--- app/src/main/java/com/phonepe/notely/ui/adapter/FilterAdapter.kt
+++ app/src/main/java/com/phonepe/notely/ui/adapter/FilterAdapter.kt
@@ -5,10 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.TextView
-
 import com.phonepe.notely.R
-import com.phonepe.notely.ui.model.Filter
+import com.phonepe.notely.databinding.FilterItemBinding
+import android.databinding.DataBindingUtil.inflate
 
 /**
  * Created by Kumar Gaurav on 1/24/2018.
@@ -18,23 +17,9 @@ import com.phonepe.notely.ui.model.Filter
 class FilterAdapter(private val mContext: Context, private val mFilters: List<String>) : ArrayAdapter<String>(mContext, R.layout.filter_item, mFilters) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var convertView = convertView
-        val holder: ViewHolder
-        if (convertView == null) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.filter_item, parent, false)
-            holder = ViewHolder()
-            holder.filter = convertView!!.findViewById(R.id.filter)
-            convertView.tag = holder
-        } else {
-            holder = convertView.tag as ViewHolder
-        }
-
-        val filter = mFilters[position]
-        holder.filter!!.text = filter
-        return convertView
-    }
-
-    internal class ViewHolder {
-        var filter: TextView? = null
+        val inflater = LayoutInflater.from(parent.context)
+        val binding: FilterItemBinding = inflate(inflater, R.layout.filter_item, parent, false)
+        binding.setFilter(mFilters[position])
+        return binding.root
     }
 }
